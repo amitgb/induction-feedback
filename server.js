e02726c70134107e268c33b588f594fefeb2719a
amitgb <AB00364299@INPUHJLP05938.TechMahindra.com>
2015-08-27 17:58:17 +0530

    port ip changed

--- server.js
+++ server.js
@@ -27,5 +27,8 @@ app.post('/feedback', function(req,res){
   });
 });
 
-app.listen(3000)
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(port, ip)
 console.log("NODE.js Server running on port 3000");
